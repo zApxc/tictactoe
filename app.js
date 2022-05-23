4cf86602025a8c66bfc1f4fec7e668bda8cd6efc
Luca Capecchi <lucacapecchi04@gmail.com>
2022-05-23 17:57:21 +0200

    Add game cells to the grid

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const blessed = require('blessed')
 const { Game } = require('./game')
 
-let screen = blessed.screen({
+const screen = blessed.screen({
   smartCSR: true,
 })
 
-screen.key(['escape', 'q', 'C-c'], function (ch, key) {
+screen.key(['escape', 'q', 'C-c'], (ch, key) => {
   return process.exit(0)
 })
 
@@ -13,40 +13,42 @@ let commands = blessed.box({
   top: 'left',
   left: 'left',
   width: '200',
-  height: '240',
+  height: '300',
+  align: 'center',
+  valign: 'middle',
+  content:
+    'Commands\n\n' +
+    '{bold}q, ESC, Ctrl-C{/bold} - Quit\n' +
+    '{bold}w, k, up (↑){/bold} - Move up\n' +
+    '{bold}s, j, down (↓){/bold} - Move down\n' +
+    '{bold}a, h, left (←){/bold} - Move left\n' +
+    '{bold}d, l, right (→){/bold} - Move right\n' +
+    '{bold}ENTER, Left click{/bold} - Place X/O\n',
   tags: true,
   border: {
     type: 'line',
   },
   style: {
     fg: 'white',
-    bg: '#111111',
     border: {
       fg: 'white',
     },
   },
 })
 
-commands.setContent(
-  '{center}Commands\n\n' +
-    '{bold}q, ESC, Ctrl-C{/bold} - Quit\n' +
-    '{bold}w, k, up (↑){/bold} - Move up\n' +
-    '{bold}s, j, down (↓){/bold} - Move down\n' +
-    '{bold}a, h, left (←){/bold} - Move left\n' +
-    '{bold}d, l, right (→){/bold} - Move right\n' +
-    '{bold}ENTER, Left click{/bold} - Place X/O\n' +
-    '{/center}'
-)
-
 let game = new Game(screen)
 
-screen.append(game.game)
+screen.append(game.grid)
 screen.append(commands)
-
-game.game.focus()
 
 screen.render()
 
 game.drawX(1)
 game.drawO(2)
 game.drawO(3)
+game.drawX(4)
+game.drawO(5)
+game.drawO(6)
+game.drawX(7)
+game.drawO(8)
+game.drawX(9)
